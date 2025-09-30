90297927dd133ddc4e7c50b2fd546d34516506f5
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2025-09-30 21:08:39 +0800

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import Providers from "./provider";
-import LoginModal from "@/components/LoginModal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +19,40 @@ export const metadata: Metadata = {
     default: "Stay Kost",
   },
   description: "Stay Kost - Cari & Kelola Kost Lebih Mudah",
-  // metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+  keywords: [
+    "kost",
+    "sewa kost",
+    "kost murah",
+    "kost online",
+    "kost dekat kampus",
+    "kost nyaman",
+  ],
+  authors: [{ name: "Kevin Pandoh" }],
+  metadataBase: new URL("https://www.staykost.my.id"),
+  openGraph: {
+    title: "Aplikasi Penyewaan Kost Online | StayKost",
+    description:
+      "Cari dan temukan kost dengan mudah, cepat, dan sesuai kebutuhanmu.",
+    url: "https://www.staykost.my.id",
+    siteName: "StayKost",
+    images: [
+      {
+        url: "https://www.staykost.my.id/og-image.jpg", // bikin gambar promosi kostmu
+        width: 1200,
+        height: 630,
+        alt: "Stay Kost - Aplikasi Penyewaan Kost",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Aplikasi Penyewaan Kost Online | Stay Kost",
+    description: "Cari kost murah & nyaman lewat aplikasi KostKu.",
+    images: ["https://www.staykost.my.id/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Stay Kost - Aplikasi Penyewaan Kost",
+    short_name: "Stay Kost",
+    description: "Stay Kost - Cari & Kelola Kost Lebih Mudah",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
